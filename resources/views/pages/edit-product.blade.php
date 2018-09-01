1c4ff56021a6eb4b9d6f7665d795471ff8d45022
Huong <admin@Apples-MacBook-Pro.local>
2018-09-01 20:18:36 +0700

    view

--- resources/views/pages/edit-product.blade.php
+++ resources/views/pages/edit-product.blade.php
@@ -1,29 +1,16 @@
 @extends('pages.layout')
 @section('content')
 <div class="panel panel-default">
-    <div class="panel-heading"><b>Cập nhật thông tin sản phẩm ...</b>
+    <div class="panel-heading">
+        <b>Cập nhật thông tin sản phẩm <i>{{$product->name}}</i></b>
     </div>
     <div class="panel-body">
         <form class="" action="">
             <div class="col-sm-6">
                 <div class="form-group">
                     <label>Name:</label>
-                    <input type="text" class="form-control" id="name" name="name">
+                    <input type="text" class="form-control" id="name" name="name" value="{{$product->name}}">
                 </div>
-                <div class="form-group">
-                    <label>Price:</label>
-                    <input type="text" class="form-control" id="name" name="price">
-                </div>
-                <div class="form-group">
-                    <label>Promotion Price:</label>
-                    <input type="text" class="form-control" id="name" name="promotion_price">
-                </div>
-                <div class="form-group">
-                        <label>Detail:</label>
-                        <textarea class="form-control" name="detail"></textarea>
-                    </div>
-            </div>
-            <div class="col-sm-6">
                 <div class="form-group">
                     <label>Loại:</label>
                     <select name="type" class="form-control">
@@ -33,9 +20,14 @@
                     </select>
                 </div>
                 <div class="form-group">
-                    <label>Promotion:</label>
-                    <input type="text" class="form-control" id="name" name="promotion">
+                    <label>Price:</label>
+                    <input type="text" class="form-control" id="name" name="price" value="{{$product->price}}">
                 </div>
+                <div class="form-group">
+                    <label>Promotion Price:</label>
+                    <input type="text" class="form-control" id="name" name="promotion_price" value="{{$product->promotion_price}}">
+                </div>
+
                 <div class="form-group">
                     <div class="checkbox">
                         <label><input type="checkbox" name="status"> Special Product</label>
@@ -51,8 +43,19 @@
                     <br><br>
                     <input type="file" name="image">
                 </div>
+
                 <button type="submit" class="btn btn-primary">Update</button>
             
+            </div>
+            <div class="col-sm-6">
+                    <div class="form-group">
+                        <label>Detail:</label>
+                        <textarea class="form-control" name="detail" rows="8">{{$product->detail}}</textarea>
+                    </div>
+                <div class="form-group">
+                    <label>Promotion:</label>
+                    <textarea class="form-control" id="name" name="promotion" rows="8">{{$product->promotion}}</textarea>
+                </div>
             </div>
         </form>
 
